28887334e496acbf813c688a9b3e2a33b964dcb6
NicXo <nicolas.hamtane@gmail.com>
2023-11-14 08:18:56 +0100

    almost done

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,9 @@
 # Print-It WebSite v.1
+<div id="banner">
+		<img class="arrow arrow_left" src="./assets/images/arrow_left.png" alt="Arrow Left">
+        <img class="arrow arrow_right" src="./assets/images/arrow_right.png" alt="Arrow Right">
+		<img class="banner-img" src="./assets/images/slideshow/slide1.jpg" alt="Banner Print-it">
+		<p>Impressions tous formats <span>en boutique et en ligne</span></p>
+		<div class="dots">
+		</div>
+	</div>
--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,69 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const urls = [];
+const tagLines = [];
+
+const n = slides.length;
+//console.log(n);
+
+for (var i = 0; i < n; ++i){
+	urls.push(slides[i].image);
+	tagLines.push(slides[i].tagLine);
+}
+//console.log(urls);
+//console.log(tagLines);
+
+
+const urlpref = "./assets/images/slideshow/"; 
+const url0 = urlpref + urls[0];
+//console.log(url0);
+let tagLine0 = tagLines[0]
+//console.log(tagLine0);
+
+const mainImage = document.createElement("img");
+mainImage.src = url0;
+mainImage.classList = "banner-img";
+const para = document.createElement("p");
+const inpara = document.createTextNode(tagLine0);
+para.appendChild(inpara);
+const banner = document.getElementById('banner');
+banner.appendChild(mainImage);
+banner.appendChild(para);
+//console.log(banner.innerHTML);
+
+
+const arrowleft = document.querySelector(".arrow_left");
+//console.log(left);
+const arrowright = document.querySelector(".arrow_right");
+//console.log(right);
+
+
+var L = 0;
+var R = 0;
+
+arrowleft.addEventListener('click', (e) =>{
+	//alert("left arrow");
+	console.log(e.button); //output=0 => clic gauche
+	--L;
+})
+
+arrowright.addEventListener('click', (e) =>{
+	//alert("right arrow");
+	console.log(e.button); //output=0 => clic gauche
+	++R;
+})
+
+
+const dotsdiv = document.createElement("div");
+dotsdiv.classList = "dots";
+banner.appendChild(dotsdiv);
+
+for (i = 0; i < n; ++i){
+	const dotspan = document.createElement("span");
+	dotsdiv.appendChild(dotspan);
+	dotspan.classList = "dot";
+}
+
+
